f4811373353c5849e683add69fc7cd1eb2dfdf84
Michael Eichberg <mail@michael-eichberg.de>
2023-10-03 20:33:28 +0200

    added slide set 3

--- gen-slides.bash
+++ gen-slides.bash
@@ -3,3 +3,4 @@
 rst2ld/rst2ld.py "0-Introduction.rst" > "0-Introduction.rst.html"
 rst2ld/rst2ld.py "1-Introduction to Number Theory.rst" > "1-Introduction to Number Theory.rst.html"
 rst2ld/rst2ld.py "2-Classical Encryption Techniques.rst" > "2-Classical Encryption Techniques.rst.html"
+rst2ld/rst2ld.py "3-Block Ciphers.rst" > "3-Block Ciphers.rst.html"
